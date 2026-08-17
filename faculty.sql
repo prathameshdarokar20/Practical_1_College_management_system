77ff56562b354ff285791c1426bfe7e6679f9c8f
prathameshdarokar20 <prathameshdarokar20@gmail.com>
2026-08-17 20:41:12 +0530

    Create faculty.sql

---
+++ faculty.sql
@@ -0,0 +1,11 @@
+use prathamesh;
+create table faculty(
+faculty_id int primary key,
+first_name varchar(50),
+last_name varchar(50),
+salary float,
+dept_id int,
+foreign key (dept_id) references department (dept_id)
+);
+insert into faculty value (1,'Avi','Shende','10000.0',1),(2,'Simmi','Rao','12000.0',2);
+select*from faculty;
