b23f2200b3a26b0d1e136e67f09ccd289a2f2191
prathameshdarokar20 <prathameshdarokar20@gmail.com>
2026-08-31 12:35:30 +0530

    Update faculty.sql

--- faculty.sql
+++ faculty.sql
@@ -9,3 +9,6 @@ foreign key (dept_id) references department (dept_id)
 );
 insert into faculty value (1,'Avi','Shende','10000.0',1),(2,'Simmi','Rao','12000.0',2);
 select*from faculty;
+create index idx_student_dept on faculty(dept_id);
+explain select*from faculty where dept_id=1;
+show index from faculty;
